d024307d7e28920cf244efe3f10fb2de9a4f08e6
Arex <zjfbob@gmail.com>
2016-12-24 19:34:45 +0800

    hide sidebar

---
+++ Hide_IEEE_Sidebar/Hide_IEEE_Sidebar.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Hide IEEE Sidebar
+// @namespace   Hide IEEE Sidebar
+// @require     https://code.jquery.com/jquery-3.1.0.min.js
+// @include     http://ieeexplore.ieee.org/xpls/icp.jsp?arnumber=*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var sidebar = $('div.column-side-left');
+var content = $('div.column-main');
+
+// toggle_sidebar shows or hides the sidebar, updating the "show sidebar"
+// button link text to match
+function toggle_sidebar() {
+		sidebar.hide();
+	  $('div#article-nav').hide();
+		content.css('width', 'auto');
+}
+
+// sidebar is hidden by default
+toggle_sidebar();
