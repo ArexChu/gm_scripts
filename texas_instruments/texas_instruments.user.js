6bc23038540ffb7765ebe07293c6d401e28de3fe
Arex <zjfbob@gmail.com>
2017-01-12 12:17:10 +0800

    texas

---
+++ texas_instruments/texas_instruments.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        texas_instruments
+// @namespace   texas_instruments
+// @require     https://code.jquery.com/jquery-3.1.0.min.js
+// @include     http://processors.wiki.ti.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var sidebar = $('div#mw-navigation');
+var content = $('div#content');
+
+// toggle_sidebar shows or hides the sidebar, updating the "show sidebar"
+// button link text to match
+function toggle_sidebar() {
+		sidebar.hide();
+		content.css('margin-left', '0px');
+}
+
+// sidebar is hidden by default
+toggle_sidebar();
